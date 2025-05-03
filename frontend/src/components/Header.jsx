19931cb3933d1f1e42b972e3ac93cf8fcf739ee7
DongEun02 <ehddms0517@naver.com>
2025-05-03 22:51:58 +0900

    Header 컴포넌트 기본 구조

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="title">
+        <h2>영화 리뷰 플랫폼</h2>
+        <p>로그인</p>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
