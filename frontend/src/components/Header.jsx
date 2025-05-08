c3e9099694e356990e1eae0096c2f2a8a7891adc
DongEun02 <ehddms0517@naver.com>
2025-05-08 14:51:42 +0900

    Header 구현

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,24 @@
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const nav = useNavigate();
+
+  const onClickLogin = () => {
+    nav("/login");
+  };
+
+  const onClickSignUp = () => {
+    nav("/signup");
+  };
+
   return (
     <div className="header">
       <div className="title">
         <h2>영화 리뷰 플랫폼</h2>
-        <p>로그인</p>
+        <input placeholder="검색어를 입력하세요" />
+        <p onClick={onClickLogin}>로그인</p>
+        <button onClick={onClickSignUp}>회원가입</button>
       </div>
       <hr />
     </div>
