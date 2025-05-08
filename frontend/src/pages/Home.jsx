5099527b2d9f1a118124d4b3082ce527d87dd828
DongEun02 <ehddms0517@naver.com>
2025-05-08 16:01:49 +0900

    영화 API 받아오기

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Context from "../components/Context";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Context />
     </div>
   );
 };
